8dcfbc37069167a598ef32c2cee97274d69f1d7f
WaterKat <jvallecillo.dev@gmail.com>
2025-10-02 10:47:11 -0700

    organizing exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export * from "./general/index.js";
-
-export * as safely from "./safely/index.js";
-export * as json from "./json/index.js";
 export * as experimental from "./experimental/index.js";
+export * from "./general/index.js";
+export * as json from "./json/index.js";
+export * as safely from "./safely/index.js";
